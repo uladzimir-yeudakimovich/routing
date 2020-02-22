f2783bb98d9f21c1b77f2eaeba358593f723bd3b
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-02-22 14:28:50 +0300

    fetching route parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { ServersServise } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'users/:id', component: UserComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent }
 ];
 
