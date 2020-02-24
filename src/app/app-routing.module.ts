f3c340b7853f6c266826e65c86cb015923b74e59
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-02-24 23:50:54 +0300

    use # in url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,6 +33,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
+  // imports: [RouterModule.forRoot(routes, {useHash: true})],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
